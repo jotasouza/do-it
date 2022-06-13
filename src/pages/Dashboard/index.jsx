1cdc7fcf8579d0001ef464bc75908977daa8003b
JotaSouza <jotabccif@gmail.com>
2022-06-13 15:03:52 -0300

    Comentado Header

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -8,7 +8,7 @@ import { Container, InputContainer, TasksContainer } from "./styles";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import Card from "../../components/Card";
-import Header from "../../components/Header";
+// import Header from "../../components/Header";
 
 //Form
 import { useForm } from "react-hook-form";
@@ -100,7 +100,7 @@ const Dashboard = ({ userAuthenticated }) => {
 
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Container>
         <InputContainer onSubmit={handleSubmit(onHandleSubmit)}>
           <section>
